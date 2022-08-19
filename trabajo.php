aca448e48fcb9ec8ec39eb01317a2c4cb2d61f51
ervinjuan@gmail.com <ervinjuan@gmail.com>
2022-08-19 14:26:28 -0300

    valentino

---
+++ trabajo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <!---
+olechepro/olechepro is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
+
+    <title>Document</title>
+</head>
+<body>
+    <table class="table">
+        <thead>
+            <th scope="col">¿como se llama?</th>
+            <th scope="col">¿como se ve?</th>
+            <th scope="col">¿para que se usa?</th>
+            <th scope="col">¿como se codifica?</th>
+        </thead>
+    </table>
+    
+</body>
+</html>
